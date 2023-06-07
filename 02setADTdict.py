af7cd79f295ea1beabc67117d6e985f894af6ad4
Kunal05052003 <106446910+Kunal05052003@users.noreply.github.com>
2023-06-07 23:48:31 +0530

    Update 02setADTdict.py

--- 02setADTdict.py
+++ 02setADTdict.py
@@ -1,94 +1,91 @@
-class HashFunction:
-    def __init__(self):
-        self.h = [{'key': -1, 'name': 'NULL'} for _ in range(10)]
+def initialize_hash_table():
+    return [{'key': -1, 'name': 'NULL'} for _ in range(10)]
 
-    def insert(self):
-        cnt = 0
-        flag = 0
-        while True:
-            if cnt >= 10:
-                print("\n\tHash Table is FULL")
-                break
-            k = int(input("\n\tEnter a Telephone No: "))
-            n = input("\n\tEnter a Client Name: ")
-            hi = k % 10  # hash function
-            if self.h[hi]['key'] == -1:
-                self.h[hi]['key'] = k
-                self.h[hi]['name'] = n
+def insert(h):
+    cnt = 0
+    flag = 0
+    while True:
+        if cnt >= 10:
+            print("\n\tHash Table is FULL")
+            break
+        k = int(input("\n\tEnter a Telephone No: "))
+        n = input("\n\tEnter a Client Name: ")
+        hi = k % 10  # hash function
+        if h[hi]['key'] == -1:
+            h[hi]['key'] = k
+            h[hi]['name'] = n
+        else:
+            if h[hi]['key'] % 10 != hi:
+                temp = h[hi]['key']
+                ntemp = h[hi]['name']
+                h[hi]['key'] = k
+                h[hi]['name'] = n
+                for i in range(hi + 1, 10):
+                    if h[i]['key'] == -1:
+                        h[i]['key'] = temp
+                        h[i]['name'] = ntemp
+                        flag = 1
+                        break
+                for i in range(hi):
+                    if flag == 0 and h[i]['key'] == -1:
+                        h[i]['key'] = temp
+                        h[i]['name'] = ntemp
+                        break
             else:
-                if self.h[hi]['key'] % 10 != hi:
-                    temp = self.h[hi]['key']
-                    ntemp = self.h[hi]['name']
-                    self.h[hi]['key'] = k
-                    self.h[hi]['name'] = n
-                    for i in range(hi + 1, 10):
-                        if self.h[i]['key'] == -1:
-                            self.h[i]['key'] = temp
-                            self.h[i]['name'] = ntemp
-                            flag = 1
-                            break
-                    for i in range(hi):
-                        if flag == 0 and self.h[i]['key'] == -1:
-                            self.h[i]['key'] = temp
-                            self.h[i]['name'] = ntemp
-                            break
-                else:
-                    for i in range(hi + 1, 10):
-                        if self.h[i]['key'] == -1:
-                            self.h[i]['key'] = k
-                            self.h[i]['name'] = n
-                            flag = 1
-                            break
-                    for i in range(hi):
-                        if flag == 0 and self.h[i]['key'] == -1:
-                            self.h[i]['key'] = k
-                            self.h[i]['name'] = n
-                            break
-            flag = 0
-            cnt += 1
-            ans = input("\n\t..... Do You Want to Insert More Key: y/n: ")
-            if ans != 'y' and ans != 'Y':
-                break
+                for i in range(hi + 1, 10):
+                    if h[i]['key'] == -1:
+                        h[i]['key'] = k
+                        h[i]['name'] = n
+                        flag = 1
+                        break
+                for i in range(hi):
+                    if flag == 0 and h[i]['key'] == -1:
+                        h[i]['key'] = k
+                        h[i]['name'] = n
+                        break
+        flag = 0
+        cnt += 1
+        ans = input("\n\t..... Do You Want to Insert More Key: y/n: ")
+        if ans != 'y' and ans != 'Y':
+            break
 
-    def display(self):
-        print("\n\t\tKey\t\tName")
-        for i in range(10):
-            print("\n\th[{}]\t{}\t\t{}".format(
-                i, self.h[i]['key'], self.h[i]['name']))
+def display(h):
+    print("\n\t\tKey\t\tName")
+    for i in range(10):
+        print("\n\th[{}]\t{}\t\t{}".format(
+            i, h[i]['key'], h[i]['name']))
 
-    def find(self, k):
-        for i in range(10):
-            if self.h[i]['key'] == k:
-                print("\n\t{} is Found at {} Location With Name {}".format(
-                    self.h[i]['key'], i, self.h[i]['name']))
-                return i
-        print("\n\tKey Not Found")
-        return -1
+def find(h, k):
+    for i in range(10):
+        if h[i]['key'] == k:
+            print("\n\t{} is Found at {} Location With Name {}".format(
+                h[i]['key'], i, h[i]['name']))
+            return i
+    print("\n\tKey Not Found")
+    return -1
 
-    def delete(self, k):
-        index = self.find(k)
-        if index != -1:
-            self.h[index]['key'] = -1
-            self.h[index]['name'] = "NULL"
-            print("\n\tKey is Deleted")
+def delete(h, k):
+    index = find(h, k)
+    if index != -1:
+        h[index]['key'] = -1
+        h[index]['name'] = "NULL"
+        print("\n\tKey is Deleted")
 
-
-if __name__ == '__main__':
-    obj = HashFunction()
+if _name_ == '_main_':
+    h = initialize_hash_table()
     while True:
         print("\n\t***** Telephone (ADT) *****")
         print("\n\t1. Insert\n\t2. Display\n\t3. Find\n\t4. Delete\n\t5. Exit")
         ch = int(input("\n\t..... Enter Your Choice: "))
         if ch == 1:
-            obj.insert()
+            insert(h)
         elif ch == 2:
-            obj.display()
+            display(h)
         elif ch == 3:
-            k = int(input("\n\tEnter a Key Which You Want to Search: "))
-            obj.find(k)
+            find(h, k)
         elif ch == 4:
             k = int(input("\n\tEnter a Key Which You Want to Delete: "))
-            obj.delete(k)
+            delete(h, k)
         elif ch == 5:
             break
         ans = input("\n\t..... Do You Want to Continue in Main Menu: y/n: ")
